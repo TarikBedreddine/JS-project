cf0aa7ec88b9aaddbf6b6c7ab693fde2a485af7b
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-03-25 16:40:05 +0100

    feat: WIP on ajax request #7

--- scripts/script-index.js
+++ scripts/script-index.js
@@ -1,3 +1,100 @@
 $( document ).ready(function() {
-    //
+
+    $.ajax({
+        //L'URL de la requête
+        url: "https://www.spaceflightnewsapi.net/api/v2/articles",
+        //La méthode d'envoi (type de requête)
+        method: "GET",
+        //Le format de réponse attendu
+        dataType : "json",
+
+
+
+        })
+
+        .done(function(response){
+            console.log(response[2].title);
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        .fail(function(error){
+            alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
