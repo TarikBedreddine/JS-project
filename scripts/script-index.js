90ff82b1fb5a5cdcd22554f7e8b1d2b5097a7a2d
Tarik <tarik.bedreddine@le-campus-numerique.fr>
2021-03-30 16:52:18 +0200

    feat: user can fill the form to add one article fixes #19

--- scripts/script-index.js
+++ scripts/script-index.js
@@ -9,7 +9,6 @@ window.onclick = function(event) {
     if (!event.target.matches('.drop-btn')) {
       const dropdown = document.getElementsByClassName("inside-dropdown");
       var i;
-      console.log(dropdown.length)
       for (i = 0; i < dropdown.length; i++) {
         var openDropdown = dropdown[i];
         if (openDropdown.classList.contains('show')) {
@@ -31,61 +30,64 @@ $('.carousel').slick({
 
 })
 
-
 // This Array will contain all articles id's 
 const idArticles = [];
 
-//the function apiUpdate has the suffix of the URL API 
-// v2/articles display all articles - v2/articles/id display one article
+/* the function apiUpdate has the suffix of the URL API
+v2/articles display all articles - v2/articles/id display one article */
 const apiUpdate = (suffix) => {
     let apiUrl = `https://spaceflightnewsapi.net/api/v2/${suffix}`;
     fetch(apiUrl)
         .then((response) => response.json())
         .then(data => {
 
-        //If the api Response is an array then 
+        //If the api Response is an array then
         if (data.length > 0) {
-            // I send all id's to the idArticles array
+
+        // I send all id's to the idArticles array
             for (let i = 0; i < data.length; i++) {
                 idArticles.push(data[i].id)
             }
-            for (var i = 0; i < data.length; i++) {
-                $("<article>\
+                for (var i = 0; i < data.length; i++) {
+                    $("<article>\
                 <div class='flex-post'>\
                 <h2>" + data[i].title + "</h2>\
-                <span>" + new Date (data[i]['publishedAt']).toLocaleDateString('FR', { timeZone: 'UTC' }) + "</span>\
+                <span>" + new Date(data[i]['publishedAt']).toLocaleDateString('FR', {timeZone: 'UTC'}) + "</span>\
                 </div>\
                 <div class='content'>\
-                <img src=" + data[i].imageUrl + " alt=''>\
+                <img src="+ data[i].imageUrl +" alt=>\
                 <p>" + data[i]['summary'] + "</p>\
                 </div>\
                 </article>\
                 ").appendTo(".feed-article")
+                }
             }
-        } 
-        //if it's only one article to display i don't need an index
-        else {
-            $("<article>\
-                <div class='flex-post'>\
-                <h2>" + data.title + "</h2>\
-                <span>" + new Date (data['publishedAt']).toLocaleDateString('FR', { timeZone: 'UTC' }) + "</span>\
-                </div>\
-                <div class='content'>\
-                <img src=" + data.imageUrl + " alt=''>\
-                <p>" + data['summary'] + "</p>\
-                </div>\
-                </article>\
-                ").appendTo(".feed-article")
-        }
+
+            //if it's only one article to display i don't need an index
+            else {
+                $("<article>\
+                    <div class='flex-post'>\
+                    <h2>" + data.title + "</h2>\
+                    <span>" + new Date (data['publishedAt']).toLocaleDateString('FR', { timeZone: 'UTC' }) + "</span>\
+                    </div>\
+                    <div class='content'>\
+                    <img src=" + data.imageUrl + " alt=''>\
+                    <p>" + data['summary'] + "</p>\
+                    </div>\
+                    </article>\
+                    ").appendTo(".feed-article")
+            }
         });
 }
-// Call the function to get all articles
-apiUpdate("articles");
 
-// CREATE AN REFRESH ICON (random results)
+    // Call the function to get all articles
+    apiUpdate("articles");
+
+    // CREATE AN REFRESH ICON (random results)
     $('.container-main').append("<span id='refresh-span'><a href='#feed-section' ><img alt='' src='images/refresh.png'></a></span>")
     $('.container-main span a img').addClass('imageRefresh')
-// When the button refresh is clicked, a loop get random articles (thanks to the idArticles array)
+
+    // When the button refresh is clicked, a loop get random articles (thanks to the idArticles array)
     $('.imageRefresh').click(function () {
         $('.feed-article article').remove();
         let x = 0;
@@ -95,8 +97,49 @@ apiUpdate("articles");
             x++;
         }
     })
+
+    // ADD ARTICLE
+    createPost = (data) => {
+            $("<article>\
+                <div class='flex-post'>\
+                <h2>" + data.title + "</h2>\
+                <span>"+ new Date(data['publishedAt']).toLocaleDateString('FR', {timeZone: 'UTC'}) +"</span>\
+                </div>\
+                <div class='content'>\
+                <img src="+ data.imageUrl +" alt=>\
+                <p>" + data['summary'] + "</p>\
+                </div>\
+                </article>\
+                ").appendTo(".feed-article")
+    }
+    // Check if there is spaces
+    const isEmptyOrSpaces = (str) => {
+        return str === null || str.match(/^ *$/) !== null
+    }
+
+    const articleSubmit = document.getElementById("article-submit")
+    articleSubmit.onclick = function (){
+        let title = document.getElementById("title").value
+        let publishedAt = document.getElementById("publishedAt").value
+        let imageUrl = "images/" + document.getElementById('imageUrl').files[0].name
+        let summary = document.getElementById("summary").value
+
+        if (isEmptyOrSpaces(title) && isEmptyOrSpaces(publishedAt) && isEmptyOrSpaces(imageUrl) && isEmptyOrSpaces(summary)) {
+            alert("Fill the inputs form please !")
+        } else {
+            const data = {
+                title: title,
+                publishedAt: publishedAt,
+                imageUrl: imageUrl,
+                summary: summary
+            }
+            console.log(data)
+            createPost(data)
+        }
+        event.preventDefault()
+    }
 })
 
-// ADD AN ARTICLE
+
 
 
